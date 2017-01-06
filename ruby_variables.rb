6bec56da7e4ac55c14429cc52b9f108fab134565
TiaraMF <tia.fowers@gmail.com>
2017-01-06 16:11:39 -0700

    integrated skills via model generator

---
+++ ruby_variables.rb
@@ -0,0 +1,46 @@
+#!/usr/env/ruby
+
+# name = "Tiara"
+
+# Address = ["277 E Street", "Salt Lake City", "Utah", "84103"]
+
+# puts "a string"
+# p "a string"
+
+# puts "What is the your password?"
+# password = gets.chomp
+# p password
+
+# if password == "asdfasdf"
+  # true
+# else
+  # false
+#end
+
+# Local Variables
+# 10.times do 
+# x = 10
+# end
+
+# p x
+
+# Global Variables
+# 10.times do 
+# $x = 10
+# end
+
+# p $x
+
+# Instance Variables
+# @batting_average = 300
+
+# Constant
+# TEAM = "Angels"
+# TEAM = "Atheletics"
+
+# p TEAM
+
+# Class Variable
+class MyClass
+  @@teams = ["A's", "Tigers"]
+end
