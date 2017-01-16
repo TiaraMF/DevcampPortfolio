96d8820080c6f981645c211dc7e8ff57f1965129
TiaraMF <tia.fowers@gmail.com>
2017-01-16 14:08:40 -0700

    added placeholder concerns

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,10 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
+  has_many :technologies
+  accepts_nested_attributes_for :technologies
+                        reject_if: lambda { |attrs| attrs['name'].blank? }
+                        
 validates_presence_of :title, :body, :main_image, :thumb_image 
 
 def self.angular
@@ -6,4 +12,11 @@ def self.angular
 end
 
 scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
+
+after_initalize :set_defaults
+
+def set_defaults
+  self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+  self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+end
 end
