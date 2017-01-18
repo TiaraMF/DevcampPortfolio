802692910e533a82dc36fa71069dcc319c0aa329
TiaraMF <tia.fowers@gmail.com>
2017-01-18 14:26:36 -0700

    Fixing routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
 
   root to: 'pages#home'
+=======
+  
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+>>>>>>> Stashed changes
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
