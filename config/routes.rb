72c6f992d37a30d1032788ad0ecc22d60b539b31
TiaraMF <tia.fowers@gmail.com>
2017-02-02 13:05:17 -0700

    Created a comment broadcas, configured the websocket route, and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   root to: 'pages#home'
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
@@ -14,9 +13,11 @@ Rails.application.routes.draw do
   get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do 
-
     member do 
       get :toggle_status
     end
   end
+
+  mount ActionCable.server => '/cable'
+  
 end
