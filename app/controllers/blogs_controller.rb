36b228a76096d3fc7f2cd446c791f40f76f3f689
TiaraMF <tia.fowers@gmail.com>
2017-02-07 16:07:13 -0700

    vendor compiled assets

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,9 +8,9 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     if logged_in?(:site_admin)
-      @blogs = Blog.recent.page(params[:page]).per(5)
+      @blogs = Blog.all.order(created_at: :desc).page(params[:page]).per(5)
     else
-      @blogs = Blog.published.recent.page(params[:page]).per(5)
+      @blogs = Blog.Published.all.order(created_at: :desc).page(params[:page]).per(5)
     end
     @page_title = "My Portfolio Blog"
   end
@@ -18,7 +18,7 @@ class BlogsController < ApplicationController
   # GET /blogs/1
   # GET /blogs/1.json
   def show
-    if logged_in?(:site_admin) || @blog.published?
+    if logged_in?(:site_admin) || @blog.Published?
       @blog = Blog.includes(:comments).friendly.find(params[:id])
       @comment = Comment.new
 
@@ -75,10 +75,10 @@ class BlogsController < ApplicationController
   end
 
   def toggle_status
-    if @blog.draft?
-      @blog.published!
-    elsif @blog.published?
-      @blog.draft!
+    if @blog.Draft?
+      @blog.Published!
+    elsif @blog.Published?
+      @blog.Draft!
     end
         
     redirect_to blogs_url, notice: 'Post status has been updated.'
@@ -96,6 +96,6 @@ class BlogsController < ApplicationController
     end
 
     def set_sidebar_topics
-      @side_bar_topics = Topic.with_blogs
+      @side_bar_topics = Topic.with_blogs    
     end
 end
