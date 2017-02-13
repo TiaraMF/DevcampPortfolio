5af9cf6948b9d2b03f19512780f1a4fa8be09059
TiaraMF <tia.fowers@gmail.com>
2017-02-13 09:26:49 -0700

    fixing bugs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,8 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy,]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
-
+  
   def index
     @portfolio_items = Portfolio.by_position
   end
@@ -10,10 +10,10 @@ class PortfoliosController < ApplicationController
   def sort
     params[:order].each do |key, value|
       Portfolio.find(value[:id]).update(position: value[:position])
-  end
+    end
 
-  render nothing: true
-end
+    render nothing: true
+  end
 
   def angular
     @angular_portfolio_items = Portfolio.angular
@@ -28,44 +28,49 @@ end
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to show_portfolio_path(@portfolio_item), notice: 'Your portfolio item is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-  
-  def show
-  
-  end
 
   def edit
-    
   end
 
   def update
-    if @portfolio_item.update(portfolio_params)
-      redirect_to show_portfolio_path(@portfolio_item), notice: 'The record successfuly updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
-end
+  end
+
+  def show
+  end
 
   def destroy
+    # Destroy/delete the record
     @portfolio_item.destroy
-    redirect_to portfolios_path
+
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
   end
 
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
-                                      :body, 
-                                      :main_image, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      :main_image,
                                       :thumb_image,
                                       technologies_attributes: [:id, :name, :_destroy]
-                                      )
+                                     )
   end
 
   def set_portfolio_item
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,8 +1,9 @@
 class Portfolio < ApplicationRecord
-  has_many :technologies, dependent: :destroy
+  has_many :technologies
   accepts_nested_attributes_for :technologies,
                                 allow_destroy: true,
                                 reject_if: lambda { |attrs| attrs['name'].blank? }
+
   validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
@@ -16,5 +17,5 @@ class Portfolio < ApplicationRecord
     order("position ASC")
   end
 
-  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 end
